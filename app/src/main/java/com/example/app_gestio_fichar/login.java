3b1bcaa3e59ab127bdb6c94caa445a77b0d21c2b
Alejandro Aiguade Alisultanov <129412789+AlexAiguadeAlisultanov@users.noreply.github.com>
2024-02-06 13:53:01 +0100

    a

--- app/src/main/java/com/example/app_gestio_fichar/login.java
+++ app/src/main/java/com/example/app_gestio_fichar/login.java
@@ -1,14 +1,73 @@
 package com.example.app_gestio_fichar;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
 
+    TextInputEditText email;
+    TextInputEditText pass;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    Button login;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_login);
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.pass);
+        login = findViewById(R.id.login);
+        progressBar = findViewById(R.id.progressBar);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String correu, password;
+                correu = String.valueOf(email.getText());
+                password = String.valueOf(pass.getText());
+
+                if (TextUtils.isEmpty(correu)) {
+                    Toast.makeText(login.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(login.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(correu, password)
+                        .addOnCompleteListener(login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    // Inicio de sesión exitoso
+                                    Toast.makeText(login.this, "Login successful", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // Si falla el inicio de sesión, muestra un mensaje al usuario.
+                                    Toast.makeText(login.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
